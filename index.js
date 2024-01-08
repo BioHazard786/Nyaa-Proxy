93cfca7cb8a0b79ff76498dac19b8636b03c5781
BioHazard786 <bzatch70@gmail.com>
2024-01-08 22:52:57 +0530

    revert back to 2 files

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-// any other SaaS platform
-const express = require("express");
+// for vercel only
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 8000;
 const urlToProxy = "https://nyaa.si";
 
+// proxy the base path
 app.use(
-  "*",
+  "/",
   createProxyMiddleware({
     target: urlToProxy,
     changeOrigin: true,
   })
 );
 
+const port = process.env.PORT || 8000;
+
+// Express server started
 app.listen(port, () => {
   console.log(`Proxy server is running at http://localhost:${port}`);
 });
+
+// Export the Express API
+module.exports = app;
