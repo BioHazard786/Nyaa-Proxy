3b3d227f1e031b140b68118bfc2323e818c548dc
BioHazard786 <bzatch70@gmail.com>
2024-01-08 18:47:09 +0530

    Quality of life changes

--- proxy.js
+++ proxy.js
@@ -1,11 +1,11 @@
-// any other SaaS platform
-const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 8000;
 const urlToProxy = "https://nyaa.si";
 
+// proxy every path
 app.use(
   "*",
   createProxyMiddleware({
@@ -14,6 +14,10 @@ app.use(
   })
 );
 
+// Express server started
 app.listen(port, () => {
   console.log(`Proxy server is running at http://localhost:${port}`);
 });
+
+// Export the Express API
+module.exports = app;
