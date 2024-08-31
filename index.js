b492bc54e03839a5984a7571393792db3f8eb678
BioHazard786 <bzatch70@gmail.com>
2024-08-31 17:50:54 +0530

    proxied rss path also

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 // for vercel only
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {
+  createProxyMiddleware,
+  responseInterceptor,
+} = require("http-proxy-middleware");
 const express = require("express");
 
 const app = express();
 const urlToProxy = process.env.PROXYURL || "https://nyaa.si";
 const port = process.env.PORT || 8000;
 
-// proxy the base path
-app.use(
-  "/",
-  createProxyMiddleware({
-    target: urlToProxy,
-    changeOrigin: true,
-  })
-);
+// Function to determine if the URL is an RSS feed
+const isRssFeedUrl = (url) => {
+  const rssFeedPattern = /page=rss/; // Basic pattern to identify RSS feeds
+  return rssFeedPattern.test(url);
+};
+
+// Proxy middleware configuration
+const options = {
+  target: urlToProxy.toString(),
+  changeOrigin: true,
+  selfHandleResponse: true,
+  onProxyRes: responseInterceptor(
+    async (responseBuffer, proxyRes, req, res) => {
+      if (isRssFeedUrl(req.url)) {
+        const response = responseBuffer.toString("utf8");
+        const proxiedUrl = `${req.protocol}://${req.get("host")}`;
+        return response.replaceAll(urlToProxy, proxiedUrl);
+      } else {
+        return responseBuffer.toString("utf-8");
+      }
+    }
+  ),
+};
+
+app.use("/", createProxyMiddleware(options));
 
 // Express server started
 app.listen(port, () => {
--- proxy.js
+++ proxy.js
@@ -1,19 +1,41 @@
 // any other SaaS platform
 const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {
+  createProxyMiddleware,
+  responseInterceptor,
+} = require("http-proxy-middleware");
 
 const app = express();
 const port = process.env.PORT || 8000;
 const urlToProxy = process.env.PROXYURL || "https://nyaa.si";
 
-app.use(
-  "*",
-  createProxyMiddleware({
-    target: urlToProxy,
-    changeOrigin: true,
-  })
-);
+// Function to determine if the URL is an RSS feed
+const isRssFeedUrl = (url) => {
+  const rssFeedPattern = /page=rss/; // Basic pattern to identify RSS feeds
+  return rssFeedPattern.test(url);
+};
 
+// Proxy middleware configuration
+const options = {
+  target: urlToProxy.toString(),
+  changeOrigin: true,
+  selfHandleResponse: true,
+  onProxyRes: responseInterceptor(
+    async (responseBuffer, proxyRes, req, res) => {
+      if (isRssFeedUrl(req.url)) {
+        const response = responseBuffer.toString("utf8");
+        const proxiedUrl = `${req.protocol}://${req.get("host")}`;
+        return response.replaceAll(urlToProxy, proxiedUrl);
+      } else {
+        return responseBuffer.toString("utf-8");
+      }
+    }
+  ),
+};
+
+app.use("*", createProxyMiddleware(options));
+
+// Express server started
 app.listen(port, () => {
   console.log(`Proxy server is running at http://localhost:${port}`);
 });
